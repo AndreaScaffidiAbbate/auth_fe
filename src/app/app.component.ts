8acfc0101b9c60e106857fe54bf2ab4612493a0e
Andrea <andrea.guaragna@unikey.it>
2022-06-27 15:07:59 +0200

    fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,16 @@
 import { Component } from '@angular/core';
+import firebase from "firebase/compat";
+import initializeApp = firebase.initializeApp;
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDeg5zBW-6o0DYwQyxxMF112-vIswGtAY0",
+  authDomain: "auth-fe-1a672.firebaseapp.com",
+  projectId: "auth-fe-1a672",
+  storageBucket: "auth-fe-1a672.appspot.com",
+  messagingSenderId: "338342189870",
+  appId: "1:338342189870:web:b96e7070f31574d01538bf",
+  measurementId: "G-PNLQXY9HD8"
+};
 
 @Component({
   selector: 'app-root',
@@ -7,4 +19,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Auth_2022_3';
+
+  constructor() {
+    // Initialize Firebase
+    initializeApp(firebaseConfig);
+  }
 }
